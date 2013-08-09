53ab83624fa26fe880b8fdcfa20a49b98579d8b4
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-08-09 16:47:14 +0200

    debug

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
   var cradle = require('cradle');
   var db = new(cradle.Connection)('http://' + process.env.WERCKER_COUCHDB_HOST, 5984).database('cities');
-  db.create();
+
+  //db.create();
+  db.exists(function (err, exists) {
+    if (err) {
+      console.log('error', err);
+    } else if (exists) {
+      console.log('the force is with you.');
+    } else {
+      console.log('database does not exists.');
+      db.create();
+      /* populate design documents */
+    }
+  });
+
   db.save([
     { name : 'San Francisco'},
     { name : 'Amsterdam'},
