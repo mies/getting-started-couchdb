15fa9d49f56d8b57bf1cbaee1d3c480658cfbbfe
Micha Hernandez van Leuffen <m.hernandezvanleuffen@gmail.com>
2013-08-09 16:19:17 +0200

    1.0.1

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,4 @@ from fabric.api import run
 import os
 
 def host_type():
-    #run('ls -la /usr/local/var/lib/couchdb')
-    run('sudo ls -la /var/lib/couchdb')
-    run('sudo ls -la /var/couchdb')
+    run('sudo ls -la /var/lib/couchdb/1.0.1')
